969dc59e5bc3635a5812a11432ad933a6388a8fd
Christian Locker <60779171+chrisloc1@users.noreply.github.com>
2020-02-28 20:33:37 -0800

    Update 322Lab1.c

--- Lab1/322Lab1.c
+++ Lab1/322Lab1.c
@@ -1,6 +1,15 @@
-#include<stdio.h> 
-#include<stdlib.h> 
+#include <stdio.h> 
+#include <stdlib.h> 
+#include <time.h> 
+#include <sys/times.h>
+#include <sys/wait.h> 
+#include <unistd.h> 
+#include <sys/types.h> 
 
+
+
+
+// Executable Name: time-4-baby-and-me
 /*
 The program prints the number of seconds since.. 		(see time(2))
 The program creates a child process 			(see fork(2))
@@ -12,20 +21,31 @@ The process ID of its child (if applicable)		(see fork(2))
 The return status of its child (if applicable)		(see exit(3), waitpid(2))
 The program will report the following time information	(see times(2))
 user time
- system time
- user time of child
- system time of child
+system time
+user time of child
+system time of child
 The program prints the number of seconds since..	(see time(2))
 */
 
 int main() {
+	
+	clock_t times(struct tms *buffer);
 
-	/*printf("Start: %d", 1);
-	printf("PPID: %d, PID: %d", 1, 2);
-	printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d", 1, 2, 3, 4);
-	printf("User: %d, SYS: %d", 1, 2);
-	printf("CUSER: %d, CSYS: %d", 1, 2);
-	printf("STOP: %d", 1);*/
+	int ID, stat;
+	pid_t pid, cpid;
 
-	system("pause");
+	printf("Start: %ld \n", time(NULL));
+
+	if ((ID = fork()) == 0) exit(0);
+	else cpid = waitpid(pid, &stat, 0);
+
+
+	printf("PPID: %d, PID: %d \n", getppid(), getpid());
+	printf("PPID: %d, PID: %d, CPID: %d, RETVAL: %d \n", getppid(), getpid(), ID, cpid);
+	//printf("User: %jd, SYS: %jd \n", times(&tms_utime), times(&tms_stime));
+	//printf("CUSER: %jd, CSYS: %jd \n", times(&tms_cutime), times(&tms_cstime));/**/
+
+	printf("STOP: %ld \n", time(NULL));
+
+	//system("pause");
 }
