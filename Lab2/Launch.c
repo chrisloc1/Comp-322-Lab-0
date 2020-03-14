cbbee52473c51ac891393f4a83553e24df8910ea
Christian Locker <60779171+chrisloc1@users.noreply.github.com>
2020-03-14 16:26:20 -0700

    Functionized

--- Lab2/Launch.c
+++ Lab2/Launch.c
@@ -6,31 +6,36 @@
 #include <sys/types.h>
 #include <sys/wait.h> 
 
+void ProcessPID(pid_t pid, char* FileName[]);
+
 int main(int argc, char* argv[]) {
-	char *newargv[] = { NULL };
-	char *newenviron[] = { NULL };
+	
 	pid_t pid;
 	int stat;
 
-
 	pid = fork();
-
 	
-
-	if (pid == 0) {
-		execve(argv[1], newargv, newenviron);
-		exit(0);
-	}
-	else {
-		printf("CPID: %d \n", pid);
-		//printf("CPID: %d %s\n", pid, argv[2]);
-	}
-
-	newargv[0] = argv[1];
+	ProcessPID(pid, argv);
 
 	
 
 	waitpid(pid, &stat, 0);
 
 	printf("RETVAL: %d \n", stat);
+}
+
+void ProcessPID(pid_t pid, char* FileName[]) {
+	char *newargv[] = { NULL, FileName[1], NULL };
+	char *newenviron[] = { NULL };
+
+	//newargv[0] = argv[1];
+
+	if (pid == 0) {
+		execve(FileName[1], newargv, newenviron);
+		exit(0);
+	}
+	else {
+		printf("CPID: %d \n", pid);
+		//printf("CPID: %d %s\n", pid, argv[2]);
+	}
 }
