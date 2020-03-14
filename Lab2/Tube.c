3e56d4b70fdb55d81d81e2a16712807351a3cd5a
Christian Locker <60779171+chrisloc1@users.noreply.github.com>
2020-03-13 21:25:14 -0700

    Worked more on tube

--- Lab2/Tube.c
+++ Lab2/Tube.c
@@ -7,20 +7,27 @@
 #include <sys/wait.h> 
 
 int main(int argc, char* argv[]) {
+	char *newargv[] = { NULL };
+	char *newenviron[] = { NULL };
 	pid_t pid1, pid2;
 	int stat1, stat2, pipefd[2];
 
-	pipe(pipefd[2]);
+	pipe(pipefd);
 
 	pid1 = fork();
 	pid2 = fork();
 	if (pid1 != 0 && pid2 != 0) {
 		printf("CPID1: %d \nCPID2: %d \n", pid1, pid2);
+		close(pipefd[2]);
 	}
-	if (pid1 == 0) {
+	if (pid1 == 0 && pid2 != 0) {
+		dup2(pipefd[2], 1);
+		execve(argv[1], newargv, newenviron);
 		exit(0);
 	}
-	else if(pid2 == 0) {
+	else if(pid2 == 0 && pid1 != 0) {
+		dup2(pipefd[2], 1);
+		execve(argv[2], newargv, newenviron);
 		exit(0);
 	}
 	/*else{
@@ -29,6 +36,7 @@ int main(int argc, char* argv[]) {
 
 	waitpid(pid1, &stat1, 0);
 	waitpid(pid2, &stat2, 0);
-
-	printf("RETVAL1: %d \nRETVAL2: %d \n", stat1, stat2);
+	
+	if (pid1 != 0 && pid2 != 0)
+		printf("RETVAL1: %d \nRETVAL2: %d \n", stat1, stat2);
 }
