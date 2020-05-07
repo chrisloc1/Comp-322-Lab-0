7a7d4ae0c206c187cf922094adc555641f9e0097
chrisloc1 <60779171+chrisloc1@users.noreply.github.com>
2020-05-07 05:37:01 -0700

    Really close, need to execve

--- Lab6/spiritd.c
+++ Lab6/spiritd.c
@@ -10,8 +10,8 @@
 
 void handleSig(int sig);
 
-pid_t MOLE1, MOLE2;
-
+pid_t MOLE1 = 0, MOLE2 = 0;
+int VAL = 0;
 
 int main(int argc, char* argv[]) {
 	int fd0, fd1, fd2;
@@ -40,20 +40,22 @@ int main(int argc, char* argv[]) {
 	getrlimit(RLIMIT_NOFILE, &lim);
 	printf("before: %ld  %ld\n", lim.rlim_cur, lim.rlim_max);
 
+	signal(SIGTERM, handleSig);
+	signal(SIGUSR1, handleSig);
+	signal(SIGUSR2, handleSig);
+
 	for (int i = 0; i <= lim.rlim_max; i++) {
-		close(i);
+		//close(i);
 	}
 
 	fd0 = open("/dev/null", O_RDWR);
 	fd1 = dup(0);
 	fd2 = dup(0);
 
-	//getrlimit(RLIMIT_NOFILE, &lim);
-	//printf("after: %ld  %ld\n", lim.rlim_cur, lim.rlim_max);
-
-	signal(SIGTERM, handleSig);
-	signal(SIGUSR1, handleSig);
-	signal(SIGUSR2, handleSig);
+	while (VAL != 1) {
+		pause();
+		//printf("test \n");
+	}/**/
 
 	return 0;
 }
@@ -62,23 +64,34 @@ void handleSig(int sig){
 	char* newenviron[] = { NULL };
 	char* newargv[] = { NULL, NULL, NULL };
 	srand(time(NULL));
+	pid_t pid;
 
 	if (sig == 10 || sig == 12) {
-		if (sig == 10) kill(MOLE1, SIGQUIT);
-		if (sig == 12) kill(MOLE2, SIGQUIT);
-
+		printf("USR1 signal recieved \n");
+		//if (sig == 10) kill(MOLE1, SIGQUIT);
+		//if (sig == 12) kill(MOLE2, SIGQUIT);
 		if (rand() % 2 == 0) {
-			MOLE1 = fork();
-			newargv[1] = "mole1";
-			execve("mole.c", newargv, newenviron);
+			printf("test1 \n");
+			pid = MOLE1 = fork();
+			if (pid == 0) {
+				newargv[1] = "./mole.o";
+				execve("./mole.o", newargv, newenviron);
+				//char* argv[] = { "gcc", "-c", "-o", "/abc/molw.o", "/abc/mole.c", 0 };
+				//execvp(argv[0], argv);
+			}
 		}
 		if (rand() % 2 == 1) {
-			MOLE2 = fork();
-			newargv[1] = "mole2";
-			execve("mole.c", newargv, newenviron);
+			printf("test2 \n");
+			pid = MOLE2 = fork();
+			if (pid == 0) {
+				newargv[1] = "./mole.o";
+				execve("./mole.o", newargv, newenviron);
+			}
 		}
 	}
 	else if (sig == 15) {
+		printf("Term signal recieved, terminating \n");
 		kill(0, SIGQUIT);
+		VAL = 1;
 	}
 }
