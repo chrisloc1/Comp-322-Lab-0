84d57363e79e8fe45988945c6d104f997e12a1eb
chrisloc1 <60779171+chrisloc1@users.noreply.github.com>
2020-05-06 18:12:14 -0700

    Almost done

--- Lab6/spiritd.c
+++ Lab6/spiritd.c
@@ -3,9 +3,15 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
-#include <sys/time.h>
+#include <time.h>
 #include <sys/resource.h>
 #include <fcntl.h>
+#include <signal.h>
+
+void handleSig(int sig);
+
+pid_t MOLE1, MOLE2;
+
 
 int main(int argc, char* argv[]) {
 	int fd0, fd1, fd2;
@@ -16,8 +22,7 @@ int main(int argc, char* argv[]) {
 	umask(0);
 
 	pid = fork();
-	if (pid < 0)
-	{
+	if (pid < 0){
 		printf("fork issue\n");
 		exit(1);
 	}
@@ -25,6 +30,8 @@ int main(int argc, char* argv[]) {
 		printf("cpid %d\n", pid);
 		exit(0);
 	}
+
+	//printf("%d\n", getppid());
 
 	sid = setsid();
 	if (sid < 0) exit(1);
@@ -34,17 +41,44 @@ int main(int argc, char* argv[]) {
 	printf("before: %ld  %ld\n", lim.rlim_cur, lim.rlim_max);
 
 	for (int i = 0; i <= lim.rlim_max; i++) {
-		//fcopy = dup(i);
 		close(i);
-		//fcopy = open()
 	}
 
 	fd0 = open("/dev/null", O_RDWR);
 	fd1 = dup(0);
 	fd2 = dup(0);
 
-	getrlimit(RLIMIT_NOFILE, &lim);
-	printf("after: %ld  %ld\n", lim.rlim_cur, lim.rlim_max);
+	//getrlimit(RLIMIT_NOFILE, &lim);
+	//printf("after: %ld  %ld\n", lim.rlim_cur, lim.rlim_max);
+
+	signal(SIGTERM, handleSig);
+	signal(SIGUSR1, handleSig);
+	signal(SIGUSR2, handleSig);
 
 	return 0;
+}
+
+void handleSig(int sig){
+	char* newenviron[] = { NULL };
+	char* newargv[] = { NULL, NULL, NULL };
+	srand(time(NULL));
+
+	if (sig == 10 || sig == 12) {
+		if (sig == 10) kill(MOLE1, SIGQUIT);
+		if (sig == 12) kill(MOLE2, SIGQUIT);
+
+		if (rand() % 2 == 0) {
+			MOLE1 = fork();
+			newargv[1] = "mole1";
+			execve("mole.c", newargv, newenviron);
+		}
+		if (rand() % 2 == 1) {
+			MOLE2 = fork();
+			newargv[1] = "mole2";
+			execve("mole.c", newargv, newenviron);
+		}
+	}
+	else if (sig == 15) {
+		kill(0, SIGQUIT);
+	}
 }
