7d2e24d38ac69a5df5c8f0d7f3e704db59c351d0
chrisloc1 <60779171+chrisloc1@users.noreply.github.com>
2020-05-09 01:53:29 -0700

    Update mole.c

--- Lab6/mole.c
+++ Lab6/mole.c
@@ -2,7 +2,13 @@
 #include<stdlib.h>
 
 int main(int argc, char* argv[]) {
-	FILE* logfile = fopen("lab6.log", "w");
-	fprintf(logfile, "Pop %s \n", argv[0]);
+	FILE* logfile = fopen("/mnt/c/users/chris/source/repos/322Lab6/322Lab6/lab6.log", "a");
+	//FILE* logfile = fopen("./lab6.log", "a");
+	if (logfile == NULL) {
+		perror("Failed: did not read");
+	}
+	fprintf(logfile, "Pop %s \n", argv[1]);
+	printf("mole success, closing \n");
 	fclose(logfile);
+	return 0;
 }
