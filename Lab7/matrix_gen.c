44616dce89193e1658673deefaa61eca657e1282
chrisloc1 <60779171+chrisloc1@users.noreply.github.com>
2020-05-12 19:58:34 -0700

    Think it's good

--- Lab7/matrix_gen.c
+++ Lab7/matrix_gen.c
@@ -1,6 +1,33 @@
 #include<stdio.h> 
 #include<stdlib.h>
+#include <time.h>
+
+void MatrixGen(int size);
 
 int main(int argc, char* argv[]) {
+    int MSize = atoi(argv[1]);
 
+    MatrixGen(MSize);
+
+    return 0;
 }
+
+/**/
+void MatrixGen(int size) {
+    int randomVar;
+    srand(time(NULL));
+
+    for (int x = 0; x < size; x++) {
+        for (int y = 0; y < size; y++) {
+            randomVar = rand() % 201;
+            if (randomVar <= 100)
+                randomVar = randomVar * -1;
+            else if (randomVar > 100)
+                randomVar -= 100;
+
+            printf("%d ", randomVar);
+        }
+        printf("\n");
+    }
+}
+/**/
